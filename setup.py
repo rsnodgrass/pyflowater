192267cacd7b78b88c1f707d5e340aff8211043d
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-12-26 01:52:23 -0800

    updates

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pyflowater',
-      version='0.0.3',
+      version='0.0.4',
       packages=[ 'pyflowater' ],
       description='Python interface for Flo by Moen API',
 #      long_description=long_description,
