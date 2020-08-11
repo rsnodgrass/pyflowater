c7438f4c3d08c044c4678bfd7c191f5711ea1663
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-08-11 13:56:50 -0700

    added INTERVAL_MINUTE

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pyflowater',
-      version='0.3.1',
+      version='0.3.2',
       packages=[ 'pyflowater' ],
       description='Python interface for Flo by Moen API',
 #      long_description=long_description,
